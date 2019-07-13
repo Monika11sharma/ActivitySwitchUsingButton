4268eb0eba587be527e4d9d0860e3f052da64528
sahil <sahil91098singh@gmail.com>
2019-07-13 18:53:16 +0530

    Simple Activity Switch

---
+++ app/src/main/java/com/example/activityswitch/Main2Activity.java
@@ -0,0 +1,18 @@
+package com.example.activityswitch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Toast.makeText(Main2Activity.this,"You are in Second activity now",Toast.LENGTH_SHORT).show();
+
+    }
+}
---
+++ app/src/main/java/com/example/activityswitch/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.activityswitch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this,"You are in First activity",Toast.LENGTH_SHORT).show();
+
+        button = (Button)findViewById(R.id.switchbutton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
